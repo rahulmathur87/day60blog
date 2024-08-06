2384d807149134d34824441a83980e8f8c5b227a
Rahul <rahulmathur87@gmail.com>
2024-08-07 03:52:13 +0530

    contact form email working

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, request
 import requests
+import os
+import smtplib
 
 # USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
 posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
 
 app = Flask(__name__)
+MY_EMAIL = os.environ.get("MYEMAIL")
+MY_PASSWORD = os.environ.get("PASSWORD")
 
 
 @app.route('/')
@@ -32,10 +36,15 @@ def contact():
     if request.method == "POST":
         contact_me = False
         name = request.form['name']
-        email = request.form['email']
+        user_email = request.form['email']
         phone = request.form['phone']
         message = request.form['message']
-        print(f"{name}\n{email}\n{phone}\n{message}")
+        print(f"{name}\n{user_email}\n{phone}\n{message}")
+        with smtplib.SMTP('smtp.gmail.com') as connection:
+            connection.starttls()
+            connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+            connection.sendmail(from_addr=MY_EMAIL, to_addrs=MY_EMAIL,
+                                msg=f"Subject: New contact request(Blog) - {name}\n\nName : {name}\nEmail : {user_email}\nPhone : {phone}\nMessage : {message}")
 
     return render_template('contact.html', contact_me=contact_me)
 
